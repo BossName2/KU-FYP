9312f144eb1fe3129e52ae6d3fe22bb713f14d22
George Carter <46428450+BossName2@users.noreply.github.com>
2023-10-02 13:56:10 +0100

    Changed map size
    Added map into a card container and played around with the map size

--- src/components/views/Home.jsx
+++ src/components/views/Home.jsx
@@ -1,20 +1,23 @@
 import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import { CardContainer, Card } from "../UI/Card.jsx";
 import "./Home.scss";
 import "leaflet/dist/leaflet.css";
 
 function Home() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <CardContainer>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </CardContainer>
   );
 }
 
