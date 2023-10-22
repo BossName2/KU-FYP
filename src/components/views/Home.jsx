eb349e7be426abb422107bea04c815e3eadb6f10
George Carter <46428450+BossName2@users.noreply.github.com>
2023-10-22 12:52:48 +0100

    Remove leaflet
    removed map data and leaflet packages as the app will not long be using a map

--- src/components/views/Home.jsx
+++ src/components/views/Home.jsx
@@ -1,24 +1,8 @@
-import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
-import { CardContainer, Card } from "../UI/Card.jsx";
 import "./Home.scss";
-import "leaflet/dist/leaflet.css";
+import { CardContainer } from "../UI/Card";
 
 function Home() {
-  return (
-    <CardContainer>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </CardContainer>
-  );
+  return <CardContainer></CardContainer>;
 }
 
 export default Home;
