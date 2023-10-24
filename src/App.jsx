e12cc6bb6fdea3b60e0288da299176fa34c2ff3f
George Carter <46428450+BossName2@users.noreply.github.com>
2023-10-24 14:02:24 +0100

    Colour and style
    Changed colours and contextual info like logged in user and title to make sense in terms of this app

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/layout/Layout.jsx';
-import Home from './components/views/Home.jsx';
-import Modules from './components/views/Modules.jsx';
-import Students from './components/views/Students.jsx';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout.jsx";
+import Home from "./components/views/Home.jsx";
+import Modules from "./components/views/Modules.jsx";
+import Students from "./components/views/Students.jsx";
 
 function App() {
-  const loggedInUser = 'Graeme';
+  const loggedInUser = "ModuleLeader";
 
   return (
     <BrowserRouter>
       <Layout loggedInUser={loggedInUser}>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/modules' element={<Modules />} />
-          <Route path='/students' element={<Students />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/modules" element={<Modules />} />
+          <Route path="/students" element={<Students />} />
         </Routes>
       </Layout>
     </BrowserRouter>
